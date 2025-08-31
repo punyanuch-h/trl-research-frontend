05b45c3d001e65db669def97c7a178ffabb0c8f7
Sitthida Srithanakrittathikarn <sitthida.sri@student.mahidol.edu>
2025-08-31 21:25:32 +0700

    fix review code

--- src/pages/ResearcherDetail.tsx
+++ src/pages/ResearcherDetail.tsx
@@ -300,7 +300,7 @@ export default function ResearcherDetail() {
         </Card>
         <br />
         <Card>
-          <CardContent className="space-y-6">
+          <CardContent className="space-y-6 mt-">
             {/* Research Details */}
             <Section title="Research Details">
               <ul className="space-y-1 text-muted-foreground">
