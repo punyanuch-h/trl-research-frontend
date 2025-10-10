ac0d9acb2c711dddb65684e69f216d2e5941f613
akikonine <yipoonMaNea@proton.me>
2025-10-10 09:23:42 +0700

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import Profile from "./pages/Profile";
@@ -13,7 +14,7 @@ import AppointmentDetail from "./pages/AppointmentDetail";
 import Dashboard from "./pages/Dashboard";
 import ResearcherDetail from "./pages/ResearcherDetail";
 import ResearcherForm from "./pages/researchDetails/ResearcherForm";
-import TrlScore from './pages/TrlScore';
+import TrlScore from "./pages/TrlScore";
 import Step1 from "./pages/steps/Step1";
 import Step2 from "./pages/steps/Step2";
 import Step3 from "./pages/steps/Step3";
@@ -25,24 +26,18 @@ import Step8 from "./pages/steps/Step8";
 import Step9 from "./pages/steps/Step9";
 import CompletePage from "./pages/CompletePage";
 import NotFound from "./pages/NotFound";
-import DifyChatIframe from '@/components/DifyChatIFrame';
-
-// MOCK
-import type { UserItem } from './types/user';
-import type { TRLItem } from './types/trl';
-import mockUser from './mockData/mockUser';
-import mockTRL from './mockData/mockTRL';
+import DifyChatIframe from "@/components/DifyChatIFrame";
 
 const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <DifyChatIframe />
-      <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <DifyChatIframe />
+
         <Routes>
           <Route path="/" element={<StartPage />} />
           <Route path="/login" element={<LoginPage />} />
@@ -50,6 +45,7 @@ const App: React.FC = () => {
           <Route path="/profile" element={<Profile />} />
           <Route path="/admin-homepage" element={<AdminHomePage />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          {/* <Route path="/appointment-detail" element={<AppointmentDetail />} /> */}
           <Route path="/researcher-detail" element={<ResearcherDetail />} />
           <Route path="/researcher-form" element={<ResearcherForm />} />
           <Route path="/trl-score" element={<TrlScore />} />
@@ -63,12 +59,11 @@ const App: React.FC = () => {
           <Route path="/trl-8" element={<Step8 />} />
           <Route path="/trl-9" element={<Step9 />} />
           <Route path="/complete" element={<CompletePage />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* Catch-all route */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+      </TooltipProvider>
+    </QueryClientProvider>
   );
 };
 
