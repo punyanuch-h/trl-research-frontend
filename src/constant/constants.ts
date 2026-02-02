20c288113d8cd612aded2e1e88e0ae1dee3b2c72
Punyanuch <punyanuch.hat@student.mahidol.edu>
2026-02-02 16:24:39 +0700

    fix: constanst hard code backend

--- src/constant/constants.ts
+++ src/constant/constants.ts
@@ -1,4 +1,4 @@
 /** @format */
 export const BACKEND_HOST = window.location.origin.includes("localhost")
   ? "http://localhost:8080"
-  : "https://trl-research-backend-325350196988.asia-southeast1.run.app";
+  : "https://trl-research-backend-855321865617.asia-southeast1.run.app";
