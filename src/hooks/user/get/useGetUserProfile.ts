140afe478020ae74a49743c81f3f5f6f87eb6bef
Punyanuch <punyanuch.hat@student.mahidol.edu>
2026-02-15 00:05:06 +0700

    fix: Implement user profile and notifications display within a new header component, alongside an offline logout handler.

--- src/hooks/user/get/useGetUserProfile.ts
+++ src/hooks/user/get/useGetUserProfile.ts
@@ -3,9 +3,10 @@ import { ApiQueryClient } from "@/hooks/client/ApiQueryClient";
 
 export const useGetUserProfile = () => {
   const apiQueryClient = new ApiQueryClient(import.meta.env.VITE_PUBLIC_API_URL);
+  const token = localStorage.getItem("token");
 
   return useQuery({
-    queryKey: ["getUserProfile"],
+    queryKey: ["getUserProfile", token],
     queryFn: async () => {
       const userProfile = await apiQueryClient.useGetUserProfile();
       return userProfile;
