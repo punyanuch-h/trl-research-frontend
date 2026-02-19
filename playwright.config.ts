dd29d1f43af2734b1a1b2443972f1cfe56af0028
sanntidraa <sitthida.sri@student.mahidol.edu>
2026-02-19 13:23:50 +0700

    fix: timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,8 +20,8 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-    actionTimeout: 10000,
-    navigationTimeout: 15000,
+    actionTimeout: 15000,
+    navigationTimeout: 30000,
   },
   projects: [
     {
