45ca3e74b853fd95b42ccb5454ad3e1f51e98d9c
sanntidraa <sitthida.sri@student.mahidol.edu>
2026-02-24 22:17:43 +0700

    refactor(code): delete code not use

--- src/components/RecentActivity.tsx
+++ src/components/RecentActivity.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../../components/ui/card';
-import { Badge } from '../../components/ui/badge';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Badge } from '@/components/ui/badge';
 import { AlertCircle, CheckCircle, Clock } from 'lucide-react';
 import { Activity } from './types';
 
